2daa8dc61a83473e0e8ad4901cd96e86a2925d96
Mack Stump <mack@mackstump.com>
2025-01-15 15:31:02 -0500

    nvim: migrate to snacks.picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -15,5 +15,23 @@ return {
 ]],
       },
     },
+
+    picker = {
+      win = {
+        input = {
+          keys = {
+            ["<c-v>"] = { "edit_vsplit", mode = { "i", "n" } },
+            ["<c-x>"] = { "edit_split", mode = { "i", "n" } },
+            ["<c-e>"] = { "toggle_live", mode = { "i", "n" } },
+          },
+        },
+      },
+      list = {
+        keys = {
+          ["<c-v>"] = "edit_vsplit",
+          ["<c-x>"] = "edit_split",
+        },
+      },
+    },
   },
 }
