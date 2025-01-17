ec56c37b29da712bbf10b1fc63bd71a00e6cec7b
Mack Stump <mack@mackstump.com>
2025-01-17 15:05:41 -0500

    nvim: add autocmd to disable single quote pairs in rust files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local function augroup(name)
+  return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("rust_disable_single_quote_pairs"),
+
+  pattern = "rust",
+  callback = function()
+    vim.keymap.set("i", "'", "'", { buffer = true })
+  end,
+})
